477e2f159dedc71ff2ca29bd7e075d2b44ccdb6b
SimGuo <1400012963@pku.edu.cn>
2017-04-16 10:37:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,13 @@
 using namespace std;
 using namespace cv;
 
-const string RootPath = "F://Current/MachineLearning/imagedata";
+
+const string RootPath = "C://Users/Lenovo/Desktop/images/training";
+//const string RootPath = "F://Current/MachineLearning/imagedata";
 
 string ClassPath[19] = { "/car", "/city", "/dog", "/earth", "/fireworks", "/flowers", "/fruits",
 "/glass", "/gold", "/gun", "/goldenfish", "/shoe", "/teapot", "/bear", "/dragonfly", "/football", "/plane", "/sky", "/worldcup" };
+int wordCount = 90; //生成多少个单词
 
 int WriteData(string fileName, cv::Mat& matData) {
 	
@@ -54,11 +57,15 @@ int main(int argc, char* argv[]){
 
 
 	/*
-	 *	SIFT特征探测器 
-	 *	SIFT特征提取成向量的结构
+	 *	SIFT、SURF特征探测器 
+	 *	SIFT特征和SURF提取成向量的结构
+	 *	allDescriptors中是所有的图片对应的sift特征的并集
 	 */
 	SiftFeatureDetector  siftdtc;
-	SiftDescriptorExtractor extractor;							
+	SurfFeatureDetector surfdtc;
+	SiftDescriptorExtractor siftextract;
+	SurfDescriptorExtractor surfextract;
+	Mat allDescriptors; 
 
 	/*	
 	 *	提取图片的SIFT特征到txt文件的完整流程
@@ -75,12 +82,22 @@ int main(int argc, char* argv[]){
 		}
 		do {
 			if (fileInfo.name[0] != ClassPath[i][1]) continue;
-			Mat tmp_image = imread(TmpPath +"/" + fileInfo.name);		//读入图片
+			Mat tmp_image = imread(TmpPath +"/" + fileInfo.name);
+			printf("%s\n", fileInfo.name);
 			vector<KeyPoint> tmp_keypoint;
-			siftdtc.detect(tmp_image, tmp_keypoint);							//用探测器探测到SIFT，但keypoint只是点的位置，还没有处理成向量
-			Mat tmp_descriptor;												//将对应的特征内容写入mat
-			extractor.compute(tmp_image, tmp_keypoint, tmp_descriptor);
+			//用探测器探测到SIFT，但keypoint只是点的位置，还没有处理成向量
+			siftdtc.detect(tmp_image, tmp_keypoint);					
+			//将对应的特征内容写入mat
+			Mat tmp_descriptor;												
+			siftextract.compute(tmp_image, tmp_keypoint, tmp_descriptor);
+			allDescriptors.push_back(tmp_descriptor);
 
+			//  vconcat（B,C,A）; // 等同于A=[B ;C]
+			//	hconcat（B,C,A）; // 等同于A=[B  C]
+
+			
+			/*
+				//写入文件
 			char filename[30];
 			_splitpath(fileInfo.name, NULL, NULL, filename, NULL);
 			String outPath;
@@ -88,10 +105,63 @@ int main(int argc, char* argv[]){
 			cout << outPath << " ";
 			outPath = TmpPath + "/sift/" + outPath + ".csv";
 			cout << WriteData(outPath, tmp_descriptor) << endl; //将mat内容写入文件
+			*/
 
 		} while (_findnext(hFile, &fileInfo) == 0);
 		_findclose(hFile);		
 	}
+
+
+	/*
+	 *	对 Feature 聚类构建单词本
+	 *	BuildVocabulary
+	 *	wordCount是指生成多少个单词，然后放在vocabulary里面
+	 */
+	BOWKMeansTrainer bowTrainer(wordCount);
+	Mat vocabulary = bowTrainer.cluster(allDescriptors);
+	printf("%d %d\n", vocabulary.cols, vocabulary.rows);
+
+
+
+	/*
+	 *	对原训练集合中的每张图进行特征转换，转换为词频向量。
+	 *	维数为wordCount维
+	 *	用map分类
+	 *	此段功能：准备训练数据在train_features中
+	 */
+	//定义好bow特征提取和词汇本
+	map<int, Mat> train_features;
+	Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create("SIFT");
+	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce");
+	BOWImgDescriptorExtractor bowextract(extractor, matcher);
+	bowextract.setVocabulary(vocabulary);
+	//对每幅图生成一个向量
+	for (int i = 0; i < 19; i++){
+		String TmpPath = RootPath + ClassPath[i];
+		long hFile = 0;
+		struct _finddata_t fileInfo;
+		string pathName, exdName;
+		if ((hFile = _findfirst(pathName.assign(TmpPath).append("\\*").c_str(), &fileInfo)) == -1) {
+			return 0;
+		}
+		do {
+			if (fileInfo.name[0] != ClassPath[i][1]) continue;
+			Mat tmp_image = imread(TmpPath + "/" + fileInfo.name);
+			vector<KeyPoint> tmp_keypoint;
+			siftdtc.detect(tmp_image, tmp_keypoint);
+			Mat tmp_bowdescriptor;
+			bowextract.compute(tmp_image, tmp_keypoint, tmp_bowdescriptor);//tmp_descriptor就是每张图的bow直方图表示
+			//归一化  
+			//normalize(BOWdescriptor, BOWdescriptor, 1.0, 0.0, NORM_MINMAX);
+			train_features[i].push_back(tmp_bowdescriptor); //train_features准备训练数据
+		} while (_findnext(hFile, &fileInfo) == 0);
+		_findclose(hFile);
+	}
+
+
+	/*
+	 *	训练分类器
+	 */
 
 
 
@@ -104,7 +174,8 @@ int main(int argc, char* argv[]){
 		训练过程可以首先使用cv:: FlannBasedMatcher来优化，为 descriptor建立索引树，
 		这种操作将在匹配大量数据时发挥巨大作用（比如在上百幅图像的数据集中查找匹配图像）。
 		而 Brute-force matcher在这个过程并不进行操作，它只是将train descriptors保存在内存中。
-
+	*/
+	/*
 	FlannBasedMatcher matcher;
 	//BruteForceMatcher<L2<float>> matcher;
 	vector<DMatch> matches;
